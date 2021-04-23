3003c202788528aff586a4da58d56b3283bafb1c
Taly <vitalik7tv@yandex.ru>
2021-04-23 18:54:27 +0300

    Add check for k-index

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ const BOT_TOKEN = process.env.BOT_TOKEN;
 const ALERTS_URL = process.env.ALERTS_URL;
 const DISCUSSION_URL = process.env.DISCUSSION_URL;
 const SOLAR_MAR_URL = process.env.SOLAR_MAR_URL;
+const K_INDEX_FORECAST_URL = process.env.K_INDEX_FORECAST_URL;
 const SCHEDULE = process.env.SCHEDULE;
 
 const TelegramBot = require('node-telegram-bot-api');
+const QuickChart = require('quickchart-js');
 const axios = require('axios');
 const cron = require('node-cron');
 const path = require('path');
@@ -30,72 +32,237 @@ const db = low(adapter);
 db.defaults({
     alerts: [],
     discussion: [],
-    synopticMap: []
+    synopticMap: [],
+    k_index: []
 }).write()
 
-cron.schedule(SCHEDULE, async () => {
-    console.log('Running a task');
+const main = async function () {
+  console.log('Running a task');
 
-    axios.get(ALERTS_URL)
-        .then(async function (response) {
-            const data = response.data;
-            const lastEvent = data[0];
+  /**
+   * Check for alerts
+   */
+  axios.get(ALERTS_URL)
+    .then(async function (response) {
+      const data = response.data;
+      const lastEvent = data[0];
 
-            // For performance, use .value() instead of .write() if you're only reading from db
-            const inDB = !!(db.get('alerts').find(lastEvent).value());
+      // For performance, use .value() instead of .write() if you're only reading from db
+      const inDB = !!(db.get('alerts').find(lastEvent).value());
 
-            if (!inDB) {
-                await db.get('alerts')
-                    .push(lastEvent)
-                    .write()
+      if (!inDB) {
+        await db.get('alerts')
+          .push(lastEvent)
+          .write()
 
-                bot.sendMessage(CHANNEL_ID, lastEvent.message);
+        bot.sendMessage(CHANNEL_ID, lastEvent.message);
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+
+  /**
+   * Check for a new discussion
+   */
+  axios.get(DISCUSSION_URL)
+    .then(async function (response) {
+      let data = response.data;
+
+      const lastEvent = {
+        // issue_datetime: '2021 Feb 21 0030 UTC',
+        message: data
+      };
+
+      // For performance, use .value() instead of .write() if you're only reading from db
+      const inDB = !!(db.get('discussion').find(lastEvent).value());
+
+      if (!inDB) {
+        await db.get('discussion')
+          .push(lastEvent)
+          .write()
+
+        bot.sendMessage(CHANNEL_ID, lastEvent.message);
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+
+  /**
+   * Check for k-index forecast
+   */
+  axios.get(K_INDEX_FORECAST_URL)
+    .then(async function (response) {
+      const data  = response.data;
+      const labels = [];
+      const kIndexes = [];
+
+      /**
+       * Remove header ["time_tag","kp","observed","noaa_scale"]
+       */
+      data.shift();
+
+      /**
+       * Remove data for past hours
+       */
+      data.filter(item => {
+        const date = new Date(item[0]);
+
+        date.setTime(date.getTime() + (3 * 60 * 60 * 1000));
+
+        return date > new Date();
+      })
+        .map(item => {
+          labels.push(item[0]);
+          kIndexes.push(item[1]);
+        });
+
+      const lastEvent = {
+        message: data
+      };
+
+      // For performance, use .value() instead of .write() if you're only reading from db
+      const inDB = !!(db.get('k_index').find(lastEvent).value());
+
+      if (!inDB) {
+        await db.get('k_index')
+          .push(lastEvent)
+          .write()
+
+        const chart = new QuickChart();
+
+        chart.setWidth(900)
+        chart.setHeight(400);
+
+        let chartUrl = 'https://quickchart.io/chart';
+
+        chart.setConfig({
+          type: 'bar',
+          data: {
+            labels: labels.map(date => {
+              const monthShortNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+              ];
+              let dateItem = new Date(date);
+
+              dateItem.setTime(dateItem.getTime() + (3 * 60 * 60 * 1000));
+
+              const DAY = `${dateItem.getDate()}`;
+              const HOUR = `${dateItem.getHours()}`;
+
+              if (HOUR === '0') {
+                return `${DAY} ${monthShortNames[dateItem.getMonth()]}`;
+              }
+
+              return `${HOUR}:00`;
+            }),
+            datasets: [{
+              data: kIndexes,
+              fill: true,
+              backgroundColor: kIndexes.map(kIndex => {
+                if (kIndex <= 3) return '#00ff00';
+                if (kIndex <= 5) return '#ffff00';
+                if (kIndex <= 9) return '#ff0000';
+              }),
+              borderWidth: 1
+            }]
+          },
+          options: {
+            legend: {
+              display: false
+            },
+            annotation: {
+              annotations: labels.map((date, index) => {
+                let dateItem = new Date(date);
+
+                dateItem.setTime(dateItem.getTime() + (3 * 60 * 60 * 1000));
+
+                const DAY = `${dateItem.getDate()}`;
+                const HOUR = `${dateItem.getHours()}`;
+
+                if (HOUR === '0') {
+                  return {
+                    type: 'line',
+                    mode: 'vertical',
+                    scaleID: 'x-axis-0',
+                    value: index,
+                    borderColor: '#aaa',
+                  };
+                }
+
+                return [];
+              })
+                .filter(value => {
+                  return value !== [];
+                }),
+            },
+            title: {
+              display: true,
+              text: 'NOAA planetary k-index forecast',
+              fontColor: '#ffffff'
+            },
+            plugins: {
+              backgroundImageUrl: 'https://capella.pics/13da7ff1-b9cc-4838-8baa-eab18b2829cf.jpg',
+            },
+            scales: {
+              yAxes: [{
+                ticks: {
+                  min: 0,
+                  max: 9,
+                  stepSize: 1,
+                  fontColor: '#aaa'
+                },
+                gridLines: {
+                  color: '#aaa'
+                }
+              }],
+              xAxes: [{
+                ticks: {
+                  fontColor: '#aaa'
+                },
+                gridLines: {
+                  color: 'transparent'
+                }
+              }]
             }
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
+          }
+        });
 
-    axios.get(DISCUSSION_URL)
-        .then(async function (response) {
-            let data = response.data;
+        const chartFileName = 'chart.png';
 
-            const lastEvent = {
-                // issue_datetime: '2021 Feb 21 0030 UTC',
-                message: data
-            };
+        await chart.toFile(chartFileName);
+        await bot.sendPhoto(CHANNEL_ID, chartFileName);
+        try { fs.unlinkSync(chartFileName) } catch(err) {}
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
 
-            // For performance, use .value() instead of .write() if you're only reading from db
-            const inDB = !!(db.get('discussion').find(lastEvent).value());
+  /**
+   * Check for handwritten solar map
+   */
+  imageHash(`${SOLAR_MAR_URL}?t=${Date.now()}`,  16, true, async (error, data) => {
+    if (error) throw error;
 
-            if (!inDB) {
-                await db.get('discussion')
-                    .push(lastEvent)
-                    .write()
+    const lastEvent = {
+      hash: data
+    };
 
-                bot.sendMessage(CHANNEL_ID, lastEvent.message);
-            }
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
+    const inDB = !!(db.get('synopticMap').find(lastEvent).value());
 
-    imageHash(`${SOLAR_MAR_URL}?t=${Date.now()}`,  16, true, async (error, data) => {
-        if (error) throw error;
+    if (!inDB) {
+      await db.get('synopticMap')
+        .push(lastEvent)
+        .write()
 
-        const lastEvent = {
-            hash: data
-        };
+      bot.sendPhoto(CHANNEL_ID, `${SOLAR_MAR_URL}?t=${Date.now()}`);
+    }
+  });
+};
 
-        const inDB = !!(db.get('synopticMap').find(lastEvent).value());
+cron.schedule(SCHEDULE, main);
 
-        if (!inDB) {
-            await db.get('synopticMap')
-                .push(lastEvent)
-                .write()
 
-            bot.sendPhoto(CHANNEL_ID, `${SOLAR_MAR_URL}?t=${Date.now()}`);
-        }
-    });
-});
 
