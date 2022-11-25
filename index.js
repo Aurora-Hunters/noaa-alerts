f955943eceb7c6f93723d42a4ea78cc12100af31
Taly <vitalik7tv@yandex.ru>
2022-11-25 15:29:06 +0300

    recolor graph

--- index.js
+++ index.js
@@ -182,14 +182,14 @@ const main = async function () {
               fill: true,
               borderColor: '#ffffff22',
               backgroundColor: kIndexes.map(kIndex => {
-                if (kIndex <= 2) return '#1e3731fa';
-                if (kIndex <= 3) return '#3c6322fa';
-                if (kIndex <= 4) return '#919733fa';
-                if (kIndex <= 5) return '#804b19fa';
-                if (kIndex <= 6) return '#58212afa';
-                if (kIndex <= 7) return '#40253bfa';
-                if (kIndex <= 8) return '#232d40fa';
-                if (kIndex <= 9) return '#000000fa';
+                if (kIndex < 3) return '#1e3731fa';
+                if (kIndex >= 3) return '#3c6322fa';
+                if (kIndex >= 4) return '#919733fa';
+                if (kIndex >= 5) return '#804b19fa';
+                if (kIndex >= 6) return '#58212afa';
+                if (kIndex >= 7) return '#40253bfa';
+                if (kIndex >= 8) return '#232d40fa';
+                if (kIndex >= 9) return '#000000fa';
               }),
               borderWidth: 1
             }]
